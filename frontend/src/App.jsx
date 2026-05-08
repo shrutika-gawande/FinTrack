161cc980c0332df2002069223db73f92e3e29e51
shrutika-gawande <shrutikagawande80@gmail.com>
2026-05-08 23:20:38 +0530

    added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import MainLayout from './layouts/MainLayout';
+import Dashboard from './pages/Dashboard';
+import Income from './pages/Income';
+import Expense from './pages/Expense';
+import Budget from './pages/Budget';
+import Analytics from './pages/Analytics';
+import Settings from './pages/Settings';
 
 function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<MainLayout/>}>
+          <Route index element={<Dashboard/>} />
+          <Route path='income' element={<Income/>} />
+          <Route path='expense' element={<Expense/>} />
+          <Route path='budget' element={<Budget/>} />
+          <Route path='analytics' element={<Analytics/>} />
+          <Route path='settings' element={<Settings/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
