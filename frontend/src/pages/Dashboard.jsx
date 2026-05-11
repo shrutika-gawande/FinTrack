85e142c0d8c85d5be357a7dffab3ecf7bb1a0a53
shrutika-gawande <shrutikagawande80@gmail.com>
2026-05-11 22:37:52 +0530

    added modal

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../styles/dashboard.css"
 import SummaryCards from '../components/dashboard/SummaryCards'
 import DashDoughnutChart from '../components/dashboard/DashDoughnutChart'
 import DashBarChart from '../components/dashboard/DashBarChart'
 import QuickBudget from '../components/dashboard/QuickBudget'
 import RecentActivity from '../components/dashboard/RecentActivity'
+import Modal from '../components/common/Modal'
 
 function Dashboard() {
-  
+  const [isOpen, setIsOpen] = useState(false);
+  const [type, setType] = useState("");
+
   return (
     <main className='main'>
       <header className='page-header'>
@@ -15,9 +18,23 @@ function Dashboard() {
           <div className='page-title'>Good day! 👋</div>
           <div className="page-subtitle">Friday, 8 May 2026</div>
         </div>
-        <div style={{display:'flex',gap:'10px',flexWrap:'wrap'}}>
-          <button className='btn btn-ghost' style={{borderColor:'rgba(56,217,169,0.3',color:'var(--green)'}}>+ Add Income</button>
-          <button className='btn btn-primary'>+ Add Expense</button>
+        <div style={{ display: 'flex', gap: '10px', flexWrap: 'wrap' }}>
+          <button className='btn btn-ghost' style={{ borderColor: 'rgba(56,217,169,0.3', color: 'var(--green)' }}
+            onClick={() => {
+              setType("income");
+              setIsOpen(true);
+            }}
+          >
+            + Add Income
+          </button>
+          <button className='btn btn-primary'
+            onClick={() => {
+              setType("expense");
+              setIsOpen(true);
+            }}
+          >
+            + Add Expense
+          </button>
         </div>
       </header>
 
@@ -33,6 +50,12 @@ function Dashboard() {
         <DashDoughnutChart />
         <DashBarChart />
       </div>
+
+    <Modal 
+      type={type}
+      isOpen={isOpen}
+      onClose={() => setIsOpen(false)}
+    />
 
     </main>
   )
