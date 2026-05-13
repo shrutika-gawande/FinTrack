e923b844204303fc82c9b27159555be644e18bfd
shrutika-gawande <shrutikagawande80@gmail.com>
2026-05-13 19:02:22 +0530

    added toaster and FinanceProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from 'react-hot-toast';
 
 import MainLayout from './layouts/MainLayout';
 import Dashboard from './pages/Dashboard';
@@ -8,20 +9,28 @@ import Expense from './pages/Expense';
 import Budget from './pages/Budget';
 import Analytics from './pages/Analytics';
 import Settings from './pages/Settings';
+import { FinanceProvider } from './context/FinanceContext';
 
 function App() {
   return (
     <BrowserRouter>
+    <FinanceProvider>
+      <Toaster
+        position="bottom-right"
+        reverseOrder={false}
+        containerStyle={{ zIndex: 99999 }} 
+      />
       <Routes>
-        <Route path='/' element={<MainLayout/>}>
-          <Route index element={<Dashboard/>} />
-          <Route path='income' element={<Income/>} />
-          <Route path='expense' element={<Expense/>} />
-          <Route path='budget' element={<Budget/>} />
-          <Route path='analytics' element={<Analytics/>} />
-          <Route path='settings' element={<Settings/>} />
+        <Route path='/' element={<MainLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path='income' element={<Income />} />
+          <Route path='expense' element={<Expense />} />
+          <Route path='budget' element={<Budget />} />
+          <Route path='analytics' element={<Analytics />} />
+          <Route path='settings' element={<Settings />} />
         </Route>
       </Routes>
+      </FinanceProvider>
     </BrowserRouter>
   )
 }
