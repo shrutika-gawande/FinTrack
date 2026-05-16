05c844a4f687e9c4ac3c216e317855eb31cae633
shrutika-gawande <shrutikagawande80@gmail.com>
2026-05-16 22:34:13 +0530

    added toast

--- frontend/src/components/budget/BudgetForm.jsx
+++ frontend/src/components/budget/BudgetForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import toast from 'react-hot-toast';
 import { useFinance } from '../../context/FinanceContext'
 
 function BudgetForm() {
@@ -24,6 +25,7 @@ function BudgetForm() {
         setBudget(Number(inputValue));
         setError("");
         setInputValue("");
+        toast.success('Budget set!')
     };
 
     return (
