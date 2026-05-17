83ddd2e788ec7cd82883dcfc9fba056f446c036a
shrutika-gawande <shrutikagawande80@gmail.com>
2026-05-18 00:44:37 +0530

    implemented headers for csv

---
+++ frontend/src/utils/csvHeaders.js
@@ -0,0 +1,28 @@
+export const incomeHeaders = [
+  { label: "ID",       key: "id"     },
+  { label: "Title",    key: "label"  },
+  { label: "Amount",   key: "amount" },
+  { label: "Source",   key: "source" },
+  { label: "Date",     key: "date"   },
+  { label: "Note",     key: "note"   },
+];
+
+export const expenseHeaders = [
+  { label: "ID",       key: "id"       },
+  { label: "Title",    key: "label"    },
+  { label: "Amount",   key: "amount"   },
+  { label: "Category", key: "category" },
+  { label: "Date",     key: "date"     },
+  { label: "Note",     key: "note"     },
+];
+
+export const allTransactionHeaders = [
+  { label: "ID",       key: "id"       },
+  { label: "Type",     key: "type"     },
+  { label: "Title",    key: "label"    },
+  { label: "Amount",   key: "amount"   },
+  { label: "Source",   key: "source"   },
+  { label: "Category", key: "category" },
+  { label: "Date",     key: "date"     },
+  { label: "Note",     key: "note"     },
+];
